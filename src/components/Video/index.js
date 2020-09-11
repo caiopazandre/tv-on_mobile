9bdbbbd493ddc0857f91bea9119576f348d247d3
Caio Paz Andre <caiopazandre@gmail.com>
2020-09-11 10:07:41 -0300

    65/5000 added warning when using the expand button on the video control

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
-import { Video } from 'expo-av'
-import VideoPlayer from 'expo-video-player'
+import { Alert } from 'react-native';
+import { Video } from 'expo-av';
+import VideoPlayer from 'expo-video-player';
 
 export default class VideoPlay extends Component {
-  
+
   render() {
     return (
-        <VideoPlayer
-          videoProps={{
-            shouldPlay: true,
-            resizeMode: Video.RESIZE_MODE_CONTAIN,
-            source: {
-              uri: this.props.link,
-            },
-          }}
-          inFullscreen={true}	
-          errorCallback={(err) => console.log('Error: ', err.message, err.type, err.obj)}
-          switchToPortrait={() => console.log('Pass your logic to switchToPortrait prop')}
-        />
+      <VideoPlayer
+        videoProps={{
+          shouldPlay: true,
+          resizeMode: Video.RESIZE_MODE_CONTAIN,
+          source: {
+            uri: this.props.link,
+          },
+        }}
+        inFullscreen={true}
+        errorCallback={(err) => console.log('Error: ', err.message, err.type, err.obj)}
+        switchToPortrait={() => Alert.alert("Aviso", "Esse recurso está indisponível no momento.")}
+      />
     );
   }
 }
